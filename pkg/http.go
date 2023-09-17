3a9675bef1fa9d96c53b132df9410aba36317984
Osagie Iyayi <osagieiyayi09@gmail.com>
2023-09-17 19:31:06 +0100

    updated the response structure

--- pkg/http.go
+++ pkg/http.go
@@ -11,6 +11,16 @@ import (
 	"github.com/vicanso/go-axios"
 )
 
+const (
+	ErrBadRequest = Err("An error occured when trying to fetch exchange rates")
+)
+
+type Err string
+
+func (e Err) Error() string {
+	return string(e)
+}
+
 const EXCHANGE_BASE_URL = "https://api.coinbase.com"
 
 func LoggerMethod(name string, key string, value string) {
@@ -18,7 +28,7 @@ func LoggerMethod(name string, key string, value string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err2 := f.WriteString(value + "\n")
+	_, err2 := f.WriteString(value + "\n\n\n")
 	if err2 != nil {
 		log.Fatal(err2)
 	}
@@ -28,26 +38,59 @@ func LoggerMethod(name string, key string, value string) {
 
 }
 
-type MyBody struct {
-	Data any `json:"data"`
+type Currency struct {
+	BTC  string
+	ETH  string
+	DOGE string
+	SOL  string
+	SHIB string
+	USDT string
+}
+type RateRep struct {
+	Rates Currency `json:"rates"`
 }
 
-func RetrieveRates(currency string) any {
+// type Data struct {
+// 	Currency string            `json:"currency"`
+// 	Rates    map[string]string `json:"rates"`
+// }
+
+// // Define a struct to represent the entire JSON response
+// type Response struct {
+// 	Data Data `json:"data"`
+// }
+
+type Response struct {
+	Data RateRep `json:"data"`
+}
+
+type Rates map[string]string
+
+func RetrieveRates(currency string) (Rates, error) {
 	path := "/v2/exchange-rates?currency="
 	url := fmt.Sprintf("%s%s%s", EXCHANGE_BASE_URL, path, currency)
 	resp, err := axios.Get(url)
 	if err != nil {
-		panic(err)
+		return Rates{}, ErrBadRequest
 	}
 	LoggerMethod("json", "response", string(resp.Data))
-	var data MyBody
+	var data Response
 	json.Unmarshal(resp.Data, &data)
-	return data
+	rates := data.Data
+	context := Rates{
+		"Bitcoin":  rates.Rates.BTC,
+		"Ethereum": rates.Rates.ETH,
+		"Dogecoin": rates.Rates.DOGE,
+		"Solana":   rates.Rates.SOL,
+		"Shiba":    rates.Rates.SHIB,
+		"Tether":   rates.Rates.USDT,
+	}
+	return context, nil
 
 }
 
 func main() {
-	currency := "BTC"
-	response := RetrieveRates(currency)
+	currency := "USD"
+	response, _ := RetrieveRates(currency)
 	fmt.Println(response)
 }
