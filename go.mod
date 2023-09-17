592aea1360957fcb1a35d0e1ca86a1734905d4d8
Osagie Iyayi <osagieiyayi09@gmail.com>
2023-09-17 18:01:20 +0100

    added dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/E-wave112/gocrypto
 
 go 1.21.0
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/monaco-io/request v1.0.16 // indirect
+	github.com/vicanso/go-axios v1.6.0 // indirect
+	github.com/vicanso/http-trace v1.1.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- pkg/http.go
+++ pkg/http.go
@@ -1,1 +1,82 @@
-package pkg
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"log/slog"
+
+	"github.com/vicanso/go-axios"
+)
+
+const EXCHANGE_BASE_URL = "https://api.coinbase.com"
+
+func LoggerMethod(name string, key string, value string) {
+	// var f interface{}
+	// f, err := os.ReadFile("info.log")
+	// if err != nil {
+	// 	createdFile, err = os.Create("info.log")
+	// }
+	f, err := os.OpenFile("info.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err2 := f.WriteString(value + "\n")
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	defer f.Close()
+	logger := slog.Default()
+	logger.Info(name, key, string(value))
+
+}
+
+// func RetrieveRates(currency string) any {
+// 	var response interface{}
+// 	path := "/v2/exchange-rates?currency="
+// 	url := fmt.Sprintf("%s%s%s", EXCHANGE_BASE_URL, path, currency)
+// 	// fmt.Println("url", url)
+// 	req := request.Client{
+// 		URL:    url,
+// 		Method: "GET",
+// 	}
+// 	resp := req.Send().Scan(&response)
+// 	if !resp.OK() {
+// 		return resp.Error()
+// 	}
+// 	fmt.Println("url", resp)
+// 	LoggerMethod("json", "response", resp.String())
+// 	return resp
+
+// }
+
+type MyBody struct {
+	Data any `json:"data"`
+}
+
+func RetrieveRates(currency string) any {
+	// var response interface{}
+	path := "/v2/exchange-rates?currency="
+	url := fmt.Sprintf("%s%s%s", EXCHANGE_BASE_URL, path, currency)
+	// fmt.Println("url", url)
+	resp, err := axios.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Println(resp.Data)
+	// }
+	// fmt.Println("url", resp)
+	LoggerMethod("json", "response", string(resp.Data))
+	var data MyBody
+	json.Unmarshal(resp.Data, &data)
+	return data
+
+}
+
+func main() {
+	currency := "BTC"
+	response := RetrieveRates(currency)
+	fmt.Printf("type % T", response)
+}
