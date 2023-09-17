525b09e98c44b74635ff99c6d0a17f9d8fa7d931
Osagie Iyayi <osagieiyayi09@gmail.com>
2023-09-17 18:12:21 +0100

    removed commented code

--- pkg/http.go
+++ pkg/http.go
@@ -14,11 +14,6 @@ import (
 const EXCHANGE_BASE_URL = "https://api.coinbase.com"
 
 func LoggerMethod(name string, key string, value string) {
-	// var f interface{}
-	// f, err := os.ReadFile("info.log")
-	// if err != nil {
-	// 	createdFile, err = os.Create("info.log")
-	// }
 	f, err := os.OpenFile("info.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -33,41 +28,17 @@ func LoggerMethod(name string, key string, value string) {
 
 }
 
-// func RetrieveRates(currency string) any {
-// 	var response interface{}
-// 	path := "/v2/exchange-rates?currency="
-// 	url := fmt.Sprintf("%s%s%s", EXCHANGE_BASE_URL, path, currency)
-// 	// fmt.Println("url", url)
-// 	req := request.Client{
-// 		URL:    url,
-// 		Method: "GET",
-// 	}
-// 	resp := req.Send().Scan(&response)
-// 	if !resp.OK() {
-// 		return resp.Error()
-// 	}
-// 	fmt.Println("url", resp)
-// 	LoggerMethod("json", "response", resp.String())
-// 	return resp
-
-// }
-
 type MyBody struct {
 	Data any `json:"data"`
 }
 
 func RetrieveRates(currency string) any {
-	// var response interface{}
 	path := "/v2/exchange-rates?currency="
 	url := fmt.Sprintf("%s%s%s", EXCHANGE_BASE_URL, path, currency)
-	// fmt.Println("url", url)
 	resp, err := axios.Get(url)
 	if err != nil {
 		panic(err)
 	}
-	// fmt.Println(resp.Data)
-	// }
-	// fmt.Println("url", resp)
 	LoggerMethod("json", "response", string(resp.Data))
 	var data MyBody
 	json.Unmarshal(resp.Data, &data)
