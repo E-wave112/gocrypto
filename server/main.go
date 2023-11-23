34ce5300302bdd2b16eaada06b36e7c1d4ce8c16
Osagie Iyayi <osagieiyayi09@gmail.com>
2023-11-23 16:41:52 +0100

    added the cron service

--- server/main.go
+++ server/main.go
@@ -6,6 +6,9 @@ import (
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/E-wave112/gocrypto/pkg"
+	"github.com/robfig/cron"
 )
 
 func getHealthCheck(w http.ResponseWriter, r *http.Request) {
@@ -14,8 +17,22 @@ func getHealthCheck(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	c := cron.New()
+
+	c.AddFunc("", func() {
+		currency := "USD"
+		rates, _ := pkg.RetrieveRates(currency)
+		// if err != nil {
+		// 	return "could not retrieve the rates at this time"
+		// }
+		fmt.Println(rates)
+	})
 	mux := http.NewServeMux()
 	mux.HandleFunc("/check", getHealthCheck)
+
+	// Start the Cron job scheduler
+	c.Start()
 
 	err := http.ListenAndServe(":9000", mux)
 	if errors.Is(err, http.ErrServerClosed) {
